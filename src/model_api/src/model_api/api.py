6e317ff6dee527e9ed427f6b4256ac4136d818b2
DenRot <dennis.rothfuss@yahoo.de>
2024-07-11 10:40:43 +0200

    first version of api

---
+++ src/model_api/src/model_api/api.py
@@ -0,0 +1,179 @@
+import os
+import pickle
+import uvicorn
+import time
+import jwt
+from pathlib import Path
+from fastapi import Request, HTTPException, Body, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from pydantic import BaseModel
+from fastapi import FastAPI
+from passlib.context import CryptContext
+
+
+# JWT settings
+JWT_SECRET = "secret"
+JWT_ALGORITHM = "HS256"
+
+# Admin credentials
+ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
+ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
+
+MODEL_PATH = "model.pkl"
+
+pwd_context = CryptContext(
+    schemes=["bcrypt"], bcrypt__default_rounds=12, deprecated="auto"
+)
+
+# In-memory users database
+users_db = {
+    "testuser": {"username": "testuser", "password": pwd_context.hash("testpassword")}
+}
+
+def load_model():
+    filehandler = open(MODEL_PATH, "rb")
+    model = pickle.load(filehandler)
+    filehandler.close()
+    return model
+
+loaded_model = load_model()
+
+# Pydantic model for user schema
+class UserSchema(BaseModel):
+    username: str
+    password: str
+
+# hashes a string password using bcrypt
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+
+# verifies if string password matches the hashed password
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+# his function constructs a JSON response with an access token
+def token_response(token: str):
+    return {"access_token": token}
+
+
+# signing a JWT token using the provided payload and secret key
+# the token expires after 100 minutes
+def sign_jwt(user_id: str):
+    payload = {
+        "user_id": user_id,
+        "expires": time.time() + 6000,
+    }  # Token expires within 100 minutes
+    token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+    return token_response(token)
+
+
+# this function decodes a JWT token to retrieve the payload
+# if the 10 minutes have not yet expired
+def decode_jwt(token: str):
+    try:
+        decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        return decoded_token if decoded_token["expires"] >= time.time() else None
+    except Exception:
+        return {}
+
+
+def check_user(data: UserSchema):
+    for user in users_db:
+        if user.username == data.username and user.password == data.password:
+            return True
+    return False
+
+
+class JWTBearer(HTTPBearer):
+    def __init__(self, auto_error: bool = True):
+        super(JWTBearer, self).__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request):
+        credentials: HTTPAuthorizationCredentials = await super(
+            JWTBearer, self
+        ).__call__(request)
+        if credentials:
+            if not credentials.scheme == "Bearer":
+                raise HTTPException(
+                    status_code=403, detail="Invalid authentication scheme."
+                )
+            if not self.verify_jwt(credentials.credentials):
+                raise HTTPException(
+                    status_code=403, detail="Invalid token or expired token."
+                )
+            return credentials.credentials
+        else:
+            raise HTTPException(status_code=403, detail="Invalid authorization code.")
+
+    def verify_jwt(self, jwtoken: str):
+        isTokenValid: bool = False
+
+        try:
+            payload = decode_jwt(jwtoken)
+        except Exception:
+            payload = None
+        if payload:
+            isTokenValid = True
+        return isTokenValid
+
+
+api = FastAPI()
+
+@api.get("/health")
+async def get_health():
+    return {"message": "Up and running!"}
+
+# endpoint for user signup that registers new users and checks if user already exists
+@api.post("/user/signup")
+async def create_user(user: UserSchema = Body(...)):
+    if user.username in users_db:
+        raise HTTPException(status_code=400, detail="User already exists")
+    hashed_password = hash_password(user.password)
+    users_db[user.username] = {"username": user.username, "password": hashed_password}
+    return sign_jwt(user.username)
+
+
+# endpoint for user login that authenticates by checking if users are admin or signed up in the user database,
+# veryfiys username and password and returns a JWT token
+@api.post("/user/login")
+async def user_login(user: UserSchema = Body(...)):
+    if ADMIN_USERNAME is None or ADMIN_PASSWORD is None:
+        raise HTTPException(
+            status_code=500,
+            detail=f"The OS env. variables `ADMIN_USERNAME` and `ADMIN_PASSWORD` have not been set!",
+        )
+
+    if user.username in users_db and verify_password(
+        user.password, users_db[user.username]["password"]
+    ):
+        return sign_jwt(user.username)
+    elif user.username == ADMIN_USERNAME and user.password == ADMIN_PASSWORD:
+        return sign_jwt(user.username)
+    raise HTTPException(status_code=401, detail="Invalid username or password")
+
+#TODO: Prediction endpoint
+"""
+@api.post("/predict", dependencies=[Depends(JWTBearer())])
+def predict_model(user_id: int):
+    users = "" #read UsersMatrix from DB
+    user = users[users["userId"].isin([user_id])]
+    user = user.drop("userId", axis=1)
+    _, indices = model.kneighbors(user)
+    # read movie titels from indices?
+    prediction = "List of movie titles?"
+    return {"prediction": prediction}
+"""
+
+@api.get("/reload_model", dependencies=[Depends(JWTBearer())])
+def reload_model():
+    global loaded_model
+    # Reload the model
+    loaded_model = load_model()
+    return {"message": "The Model was updated!"}
+
+
+if __name__ == "__main__":
+
+    uvicorn.run(api, host="0.0.0.0", port=8000)
---
+++ src/model_api/tests/api_tests.py
@@ -0,0 +1,96 @@
+import os
+from fastapi.testclient import TestClient
+from fastapi import status
+import joblib
+from unittest.mock import patch, MagicMock
+import numpy as np
+
+# Mocking the `joblib` module so no model needed for the unit tests
+mock_joblib_load = patch.object(joblib, "load", return_value=MagicMock()).start()
+
+os.environ['ADMIN_USERNAME'] = "the_admin_username"
+os.environ['ADMIN_PASSWORD'] = "the_admin_password"
+
+from model_api.api import api, users_db, ADMIN_USERNAME, ADMIN_PASSWORD, hash_password
+
+client = TestClient(api)
+
+UNKNOWN_USER = ("unknown", "user")
+NON_ADMIN_USER = list(users_db.items())[0]
+ADMIN_USER = (ADMIN_USERNAME, {"username": ADMIN_USERNAME, "password": ADMIN_PASSWORD})
+
+
+def test_health():
+    """Testing if the health endpoint works"""
+    response = client.get("/health")
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_unauthorized_login():
+    """Testing if secured endpoint doesnt work because it has no auth."""
+    response = client.get("/secured")
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+def test_unknown_user_login():
+    """Testing the secured endpoint doesnt work because of unknown user."""
+    response = client.get("/secured", auth=UNKNOWN_USER)
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+def test_login_with_known_user():
+    """Testing the userlogin endpoint works with non admin."""
+    response = client.post(
+        "/user/login", json={"username": "testuser", "password": "testpassword"}
+    )
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_login_as_admin():
+    """Testing the secured endpoint works with admin user."""
+    response = client.post(
+        "/user/login", json={"username": ADMIN_USERNAME, "password": ADMIN_PASSWORD}
+    )
+    assert response.status_code == status.HTTP_200_OK
+
+    login_data = response.json()
+    assert "access_token" in login_data
+
+    access_token = login_data["access_token"]
+
+    # Access the secured endpoint with the access token
+    secured_response = client.get(
+        "/secured", headers={"Authorization": f"Bearer {access_token}"}
+    )
+    assert secured_response.status_code == status.HTTP_200_OK
+
+#TODO: Write test for prediction endpoint
+"""
+#function to test the prediction
+@patch("model_api.api.joblib")
+def test_prediction(load_model_mock):
+    #mocking the joblib predict function
+    load_model_mock.predict.return_value = np.asarray([1])
+
+    login_response = client.post("/user/login",json= {"username": ADMIN_USERNAME, "password": ADMIN_PASSWORD})
+    assert login_response.status_code == status.HTTP_200_OK
+
+    #checking if we get an access token and saving it in the variable access token
+    login_data = login_response.json()
+    assert "access_token" in login_data
+    access_token = login_data["access_token"]
+
+    #this is the test User id
+    userId = 1
+
+    #defining the header with the previously created access token
+    headers = {"Authorization": f"Bearer {access_token}"}
+    prediction_response = client.post("/predict", userId=userId, headers=headers)
+
+    # Assert the response status code to be 200
+    assert prediction_response.status_code == status.HTTP_200_OK
+
+    # Optionally, you can check the response content
+    prediction_response_json =  prediction_response.json()
+    assert "prediction" in  prediction_response_json
+"""
