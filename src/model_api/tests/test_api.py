388e2c477724dbd8229c4211bc93cfd6f91523bc
DenRot <dennis.rothfuss@yahoo.de>
2024-07-11 10:51:24 +0200

    added init file for api package

--- src/model_api/tests/test_api.py
+++ src/model_api/tests/test_api.py
@@ -3,14 +3,13 @@ from fastapi.testclient import TestClient
 from fastapi import status
 import pickle
 from unittest.mock import patch, MagicMock
+from model_api.api import api, users_db, ADMIN_USERNAME, ADMIN_PASSWORD
 
 # Mocking the `pickle` module so no model needed for the unit tests
 mock_pickle_load = patch.object(pickle, "load", return_value=MagicMock()).start()
 
 os.environ['ADMIN_USERNAME'] = "the_admin_username"
 os.environ['ADMIN_PASSWORD'] = "the_admin_password"
-
-from model_api.api import api, users_db, ADMIN_USERNAME, ADMIN_PASSWORD, hash_password
 
 client = TestClient(api)
 
